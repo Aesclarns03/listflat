90add8775b1d506defde56a130132d5791d62828
Aesclarns03 <144084175+Aesclarns03@users.noreply.github.com>
2023-11-10 09:21:12 +0800

    flatlist

---
+++ DATA.js
@@ -0,0 +1,77 @@
+export const DATA = [
+  {
+    id: 1,
+    name: "JESSA MAE RESMA",
+    place: "Egypt, Tagbilaran"
+  },
+  {
+    id: 2,
+    name: "ALDWIN, HANDUMON",
+    place: "Canada, Jagana"
+  },
+  {
+    id: 3,
+    name: "MAGALONA, JOHNBHERT CLINT",
+    place: "Cortes, Lourdes"
+  },
+  {
+    id: 4,
+    name: "RENOBLAS, IRA",
+    place: "Japan, Hanopol"
+  },
+  {
+    id: 5,
+    name: "JAGUNOS, JOANA",
+    place: "Brazil, Hanopol"
+  },
+  {
+    id: 6,
+    name: "JAPORGS, HYKE",
+    place: "Joradan, Sinai"
+  },
+  {
+    id: 7,
+    name: "SINCHAS, ",
+    place: "Japan, Hanopol"
+  },
+  {
+    id: 8,
+    name: "WASHAMIY, DINDA",
+    place: "Nigeria, catigbian"
+  },
+  {
+    id: 9,
+    name: "LIPARTO, MONALIZA",
+    place: "Los Angeles, California"
+  },
+  {
+    id: 10,
+    name: "LIA, REFUGIO",
+    place: "Argentina, Nevada"
+  },
+  {
+    id: 11,
+    name: "SHEMY, SHEIN",
+    place: "Alaska, Bandaria"
+  },
+  {
+    id: 12,
+    name: "JESSELE, LOMOD",
+    place: "Japan, Hanopol"
+  },
+  {
+    id: 13,
+    name: "LOPAEZ, DERMA",
+    place: "France, Paris"
+  },
+  {
+    id: 14,
+    name: "DENMARK, LINDA",
+    place: "United, states"
+  },
+  {
+    id: 15,
+    name: "KING, JEYPEE",
+    place: "Japan, Hanopol"
+  },
+];
